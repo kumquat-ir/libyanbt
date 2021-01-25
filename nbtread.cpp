f689f422203b67526e2c92bbcaac2f3f43f4570b
kumquat-ir <66188216+kumquat-ir@users.noreply.github.com>
2021-01-25 00:06:27 -0800

    allow use as static library set

--- nbtread.cpp
+++ nbtread.cpp
@@ -67,16 +67,3 @@ nbtfile read_nbt(filesystem::path ifpath){
 
 	return nbtdata;
 }
-
-int main(int argc, char** argv){
-	if(argc <= 1){
-		cerr << "no file provided" << endl;
-		return 1;
-	}
-
-	nbtfile nbtdata = read_nbt(argv[1]);
-	cout << nbtdata << endl;
-	nbtdata.write_file(filesystem::temp_directory_path() / "yanbt_out.nbt");
-
-	return 0;
-}
